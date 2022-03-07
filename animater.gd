42b3256c6359581df87f6aa7236419625f0d0d51
boukew99 <boukewestra0@gmail.com>
2022-03-07 17:29:44 +0100

    added saving and screenshot

--- animater.gd
+++ animater.gd
@@ -4,13 +4,12 @@ var canvas = preload("res://addons/canvas/canvas_with_background.tscn")
 
 onready var frames = $VBoxContainer/TabContainer
 
-
 func _on_Next_pressed():
-	frames.current_tab += 1
+	frames.current_tab = clamp(frames.current_tab + 1, 0, frames.get_child_count())
 
 
 func _on_Prev_pressed():
-	frames.current_tab -= 1
+	frames.current_tab  = clamp(frames.current_tab - 1, 0, frames.get_child_count())
 
 
 func _on_Tag_text_entered(new_text):
@@ -45,3 +44,19 @@ func _on_Play_toggled(button_pressed):
 
 func _on_FPSTimer_timeout():
 	frames.current_tab = wrapi(frames.current_tab + 1, 0, frames.get_child_count())
+
+
+func _on_Save_pressed():
+	$FileDialog.popup_centered()
+
+
+func _on_FileDialog_dir_selected(dir):
+	for index in range(frames.get_child_count()):
+		frames.current_tab = index
+		get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+		yield(VisualServer, "frame_post_draw")
+
+		var image = get_viewport().get_texture().get_data()
+		image.flip_y() # capture is y-flipped
+		var frame = frames.get_child(index)
+		image.get_rect(frame.get_global_rect()).save_png(dir.plus_file(frame.name + ".png"))
