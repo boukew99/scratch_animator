529d77f303c51e972a268d4bce11f75f81636c1a
boukew99 <boukewestra0@gmail.com>
2022-05-05 22:39:03 +0200

    GUI layout, icons and naming upgrade

--- animater.gd
+++ animater.gd
@@ -2,33 +2,23 @@ extends Control
 
 var canvas = preload("res://addons/canvas/canvas_background.tscn")
 
-onready var frames = $VBoxContainer/TabContainer
+onready var frames = $VBoxContainer/HBoxContainer2/TabContainer
+onready var slider = $VBoxContainer/HBoxContainer2/VBoxContainer/VSlider
 
 func _on_Next_pressed():
-	frames.current_tab = clamp(frames.current_tab + 1, 0, frames.get_child_count())
+	frames.current_tab = wrapi(frames.current_tab +1, 0, frames.get_tab_count())
 
 
 func _on_Prev_pressed():
-	frames.current_tab  = clamp(frames.current_tab - 1, 0, frames.get_child_count())
-
-
-func _on_Tag_text_entered(new_text):
-	frames.get_current_tab_control().name = new_text
-
-
-func _on_Sub_pressed():
-	frames.get_current_tab_control().queue_free()
+	frames.current_tab = wrapi(frames.current_tab -1, 0, frames.get_tab_count())
 
 
 func _on_Add_pressed():
 	var inst = canvas.instance()
 	inst.name = "0"
 	frames.add_child(inst, true)
+	frames.move_child(inst, frames.current_tab +1)
 	_on_Next_pressed()
-
-
-func _on_TabContainer_tab_changed(tab):
-	$VBoxContainer/HBoxContainer/Tag.text = frames.get_child(tab).name
 
 
 func _on_FPS_value_changed(value):
@@ -60,4 +50,27 @@ func _on_FileDialog_dir_selected(dir):
 		image.flip_y() # capture is y-flipped
 		var frame = frames.get_child(index)
 		image.get_rect(frame.get_global_rect()).save_png(dir.plus_file(frame.name + ".png"))
+
+
+func _on_VSlider_value_changed(value):
+	frames.current_tab = wrapi(value, 0, frames.get_tab_count())
+
+
+func _on_Remove_pressed():
+	frames.get_current_tab_control().queue_free()
+
+
+func _on_Duplicate_pressed():
+	var dup = frames.get_current_tab_control().duplicate()
+	frames.add_child(dup, true)
+
+
+func _on_Rename_text_entered(new_text):
+	frames.get_current_tab_control().name = new_text
+	$VBoxContainer/HBoxContainer/Name.text = ""
+
+
+func _on_RenameOption_item_selected(index):
+	pass # Replace with function body.
+
 
