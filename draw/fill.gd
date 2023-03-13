62cce5aa864df960e2c9ffc5824755331d0f9475
boukew99 <boukewestra0@gmail.com>
2023-03-13 14:17:04 +0100

    folder-up

--- draw/fill.gd
+++
@@ -1,6 +0,0 @@
-extends "res://scratch_canvas/line.gd"
-
-func add_point_sparsely(point):
-	.add_point_sparsely(point)
-	if Geometry.merge_polygons_2d(points, $Polygon2D.polygon).size() > 0:
-		$Polygon2D.polygon = points
